a3be8fcaaaf4ad03d3f2f072e3715896d2492173
Pikay <lionelafro@gmail.com>
2023-11-02 20:58:55 +0800

    updates

---
+++ src/controllers/users.controller.ts
@@ -0,0 +1,77 @@
+import { NextFunction, Request, Response } from 'express';
+import { User } from '@prisma/client';
+import { CreateUserDto } from '@dtos/users.dto';
+import userService from '@services/users.service';
+
+class UsersController {
+  public userService = new userService();
+
+  public getUsers = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const findAllUsersData: Omit<User, 'password'>[] = await this.userService.findAllUser();
+
+      res.status(200).json({ data: findAllUsersData, message: 'findAll' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public getUserById = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const userId = Number(req.params.id);
+      const findOneUserData: Omit<User, 'password'> = await this.userService.findUserById(userId);
+
+      res.status(200).json({ data: findOneUserData, message: 'findOne' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public createUser = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const userData: CreateUserDto = req.body;
+      const createUserData: Omit<User, 'password'> = await this.userService.createUser(userData);
+
+      res.status(201).json({ data: createUserData, message: 'created' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public updateUser = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const userId = Number(req.params.id);
+      const userData: CreateUserDto = req.body;
+      const updateUserData: Omit<User, 'password'> = await this.userService.updateUser(userId, userData);
+
+      res.status(200).json({ data: updateUserData, message: 'updated' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public deleteUser = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const userId = Number(req.params.id);
+      const deleteUserData: Omit<User, 'password'> = await this.userService.deleteUser(userId);
+
+      res.status(200).json({ data: deleteUserData, message: 'deleted' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public manageGroups = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = req.body;
+      const userId = Number(req.params.id);
+      const groupData = await this.userService.manageGroups(userId, data);
+
+      res.status(200).json({ data: groupData, message: 'manageGroups' });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export default UsersController;
